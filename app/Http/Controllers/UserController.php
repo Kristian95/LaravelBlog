3d12b875aa20047ca2649b7f883afd8760523d84
Kristian <ivanov_15@mail.bg>
2018-10-23 21:20:10 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller {
 	{
 		$posts = Posts::where('author_id',$id)->where('active','1')->orderBy('created_at','desc')->paginate(5);
 		$title = User::find($id)->name;
+		
 		return view('home')->withPosts($posts)->withTitle($title);
 	}
 
@@ -22,6 +23,7 @@ class UserController extends Controller {
 		$user = $request->user();
 		$posts = Posts::where('author_id',$user->id)->orderBy('created_at','desc')->paginate(5);
 		$title = $user->name;
+		
 		return view('home')->withPosts($posts)->withTitle($title);
 	}
 	
@@ -30,26 +32,31 @@ class UserController extends Controller {
 		$user = $request->user();
 		$posts = Posts::where('author_id',$user->id)->where('active','0')->orderBy('created_at','desc')->paginate(5);
 		$title = $user->name;
+		
 		return view('home')->withPosts($posts)->withTitle($title);
 	}
 
 	public function profile(Request $request, $id) 
 	{
 		$data['user'] = User::find($id);
-		if (!$data['user'])
+		
+		if (!$data['user']) {
 			return redirect('/');
+		}
 
 		if ($request -> user() && $data['user'] -> id == $request -> user() -> id) {
 			$data['author'] = true;
 		} else {
 			$data['author'] = null;
 		}
+		
 		$data['comments_count'] = $data['user'] -> comments -> count();
 		$data['posts_count'] = $data['user'] -> posts -> count();
 		$data['posts_active_count'] = $data['user'] -> posts -> where('active', '1') -> count();
 		$data['posts_draft_count'] = $data['posts_count'] - $data['posts_active_count'];
 		$data['latest_posts'] = $data['user'] -> posts -> where('active', '1') -> take(5);
 		$data['latest_comments'] = $data['user'] -> comments -> take(5);
+		
 		return view('admin.profile', $data);
 	}
 }
